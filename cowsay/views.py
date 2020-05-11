fb2cfde6ac5bf81502019419dcb21a8266bcacdf
Rob Spears <rob.sp3ars@gmail.com>
2020-05-11 11:12:47 -0400

    Basic index view implementation rendering index.html template

--- cowsay/views.py
+++ cowsay/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from cowsay.forms import TextForm
 
 # Create your views here.
+
+
+def index(request):
+    form = TextForm()
+    return render(request, 'index.html', {'form': form})
