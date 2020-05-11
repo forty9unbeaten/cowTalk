9569a23c27760be2707d370727f5399994039d35
Rob Spears <rob.sp3ars@gmail.com>
2020-05-11 14:29:02 -0400

    Add created date to model entry and custom __str__ method

--- cowsay/models.py
+++ cowsay/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
-class TextString(models.Model):
-    text = models.CharField(max_length=150)
+class CowSentence(models.Model):
+    cow_text = models.CharField(max_length=150)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.cow_text
