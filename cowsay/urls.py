3168e732d8f0b1731c8048718cd7b1c191aac789
Rob Spears <rob.sp3ars@gmail.com>
2020-05-11 11:24:28 -0400

    Add /history url path that renders history.html page

--- cowsay/urls.py
+++ cowsay/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from cowsay import views
 
 url_paths = [
-    path('', views.index, name='homepage')
+    path('', views.index, name='homepage'),
+    path('/history', views.history_view, name='history')
 ]
