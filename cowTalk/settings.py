2ba6022578e900d82a75a9211148a3a07f3c40ce
Rob Spears <rob.sp3ars@gmail.com>
2020-05-11 14:28:05 -0400

    Edit template directory path

--- cowTalk/settings.py
+++ cowTalk/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cowTalk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/cowsay/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
