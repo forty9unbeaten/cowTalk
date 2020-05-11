c5be0afff1089aa9ab2e60b7cb10c58bb043c478
Rob Spears <rob.sp3ars@gmail.com>
2020-05-11 14:29:22 -0400

    Reformat history/ url path

--- cowsay/urls.py
+++ cowsay/urls.py
@@ -3,5 +3,5 @@ from cowsay import views
 
 url_paths = [
     path('', views.index, name='homepage'),
-    path('/history', views.history_view, name='history')
+    path('history/', views.history_view, name='history')
 ]
